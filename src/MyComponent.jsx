13ade199576cf8ed546248bc5a632a56c712aaac
Gowtham5285 <gowthamsaivalluri5285@gmail.com>
2025-12-13 16:38:13 +0530

    useState()

---
+++ src/MyComponent.jsx
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+function MyComponent(){
+    const [name,setName]=useState("Guest");
+    const [age,setAge]=useState(0);
+    const updateName=()=>{
+        setName("Spongebob");
+    }
+    const incrementAge=()=>{
+        setAge(age + 1);
+    }
+    return(
+        <div>
+            <p>Name:{name}</p>
+            <button onClick={updateName}>Set Name</button>
+
+            <p>Age:{age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
+        </div>
+    );
+
+}
+export default MyComponent
